a0ec625aaccd6aee33d1d4cb9d0b208695cc5b4e
rocketpunch <rocketpunch.dev@gmail.com>
2018-04-30 18:52:47 +0900

    Add 188

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 * [document](doc/problems_1.md) - [LINK](http://euler.synap.co.kr/prob_detail.php?id=1) / 1000보다 작은 자연수 중에서 3 또는 5의 배수를 모두 더하면? 
 * [document](doc/problems_2.md) - [LINK](http://euler.synap.co.kr/prob_detail.php?id=2) / 피보나치 수열에서 4백만 이하이면서 짝수인 항의 합?
+* [document](doc/problems_118.md) - [LINK](http://euler.synap.co.kr/prob_detail.php?id=118) / 	팬디지털 소수의 집합
---
+++ python/problems_118.py
@@ -0,0 +1,19 @@
+def get_prime(max_number):
+    number_tiles = set(range(2, max_number))
+    prime_list = list()
+
+    while len(number_tiles) > 0:
+        min_number = min(number_tiles)
+        prime_list.append(min_number)
+
+        delete_tiles = set(range(min_number, max_number, min_number))
+        number_tiles = number_tiles - delete_tiles
+
+        print(len(number_tiles))
+
+    return prime_list
+
+
+if __name__ == "__main__":
+    # print(get_prime(987654321))
+    pass
